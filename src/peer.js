39d8b1aaae834118f56d852cf496001b42aa4868
benzwjian <zwjian@gmail.com>
2017-05-10 17:40:19 +0800

    connect to peer

--- src/peer.js
+++ src/peer.js
@@ -1,9 +1,11 @@
 var net = require('net')
+var NETWORK = require('./const').NETWORK
+var PORT = require('./const').PORT
 
-var peerConnection = (ip) => {
+var peerConnection = (network) => (ip) => {
   return new Promise((resolve, reject) => {
     var client = net.connect({
-      port: 18333,
+      port: network === NETWORK.MAINNET ? PORT.MAINNET : PORT.TESTNET,
       host: ip
     }, () => {
       console.log('peer ' + ip + ' connected')
@@ -12,7 +14,6 @@ var peerConnection = (ip) => {
 
     client.on('error', (err) => {
       console.log('peer ' + ip + ' connect fail')
-      console.log(err)
       reject(err)
     })
   })
@@ -22,8 +23,8 @@ var allPeersConnected = (peers) => {
   console.log('All ' + peers.length + ' peers connected!')
 }
 
-var connect = (ips) => {
-  var allPeersConnection = ips.map(peerConnection)
+var connect = (network) => (ips) => {
+  var allPeersConnection = ips.map(peerConnection(network))
   Promise.all(allPeersConnection).then(allPeersConnected).catch(console.log)
   return allPeersConnection
 }
--- test/peer.js
+++ test/peer.js
@@ -1,11 +1,22 @@
 /* global describe, it */
 
-// var assert = require('assert')
-// var peer = require('../src/peer')
+var assert = require('assert')
+var peer = require('../src/peer')
+var NETWORK = require('../src/const').NETWORK
 
-// var Promise = require('promise')
+var Promise = require('promise')
 
 describe('Peers', function () {
-  it('should connect to 24 peers', function () {
+  it('should connect to 1 peer on Testnet', function () {
+    var peersConnectPromised = peer.connect(NETWORK.TESTNET)(['168.63.245.221'])
+    return Promise.all(peersConnectPromised).then(function (conns) {
+      assert.strictEqual(conns.length, 1)
+    })
+  })
+  it('should connect to 1 peer on Mainnet', function () {
+    var peersConnectPromised = peer.connect(NETWORK.MAINNET)(['185.50.213.123'])
+    return Promise.all(peersConnectPromised).then(function (conns) {
+      assert.strictEqual(conns.length, 1)
+    })
   })
 })
