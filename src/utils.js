ffd7d48d38eba87f0225392239b1e5d2cbf44331
benzwjian <zwjian@gmail.com>
2017-05-12 09:40:24 +0800

    add utils

---
+++ src/utils.js
@@ -0,0 +1,50 @@
+var prefixBy = (what) => (to) => {
+  var bWhat = Buffer.from(what)
+  var payload = Buffer.concat([bWhat, to], bWhat.length + to.length)
+  return payload
+}
+
+var prefixTo = (to) => (what) => {
+  var bWhat = Buffer.from(what)
+  var payload = Buffer.concat([bWhat, to], bWhat.length + to.length)
+  return payload
+}
+
+var suffixBy = (what) => (to) => {
+  var bWhat = Buffer.from(what)
+  var payload = Buffer.concat([to, bWhat], to.length + bWhat.length)
+  return payload
+}
+
+var suffixTo = (to) => (what) => {
+  var bWhat = Buffer.from(what)
+  var payload = Buffer.concat([to, bWhat], to.length + bWhat.length)
+  return payload
+}
+
+var intToHex = (integer) => {
+  var hex = (integer).toString(16)
+  hex = hex.length % 2 === 0 ? '0x' + hex : '0x0' + hex
+  return hex
+}
+
+var writeUIntBE = (length) => (integer) => {
+  var b = Buffer.alloc(length, 0)
+  b.writeUIntBE(Buffer.from(intToHex(integer)), 0, length)
+  return b
+}
+
+var writeUIntLE = (length) => (integer) => {
+  var b = Buffer.alloc(length, 0)
+  b.writeUIntLE(Buffer.from(intToHex(integer)), 0, length)
+  return b
+}
+
+module.exports = {
+  prefixBy: prefixBy,
+  prefixTo: prefixTo,
+  suffixBy: suffixBy,
+  suffixTo: suffixTo,
+  writeUIntBE: writeUIntBE,
+  writeUIntLE: writeUIntLE
+}
