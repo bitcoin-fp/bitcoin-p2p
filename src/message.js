5bb662681f66ad4602b2b17e639bc95b112e7a38
benzwjian <zwjian@gmail.com>
2017-05-23 15:03:48 +0800

    add p2p messages

--- src/message.js
+++ src/message.js
@@ -9,17 +9,12 @@ var crypto = require('crypto')
  * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#Network_address
  */
 var address = (ip, port) => {
-  var addr = Buffer.alloc(0)
-
   var service = Buffer.from([0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
   var ipHex = ip.split('.').map((digi) => '0x' + utils.writeUIntBE(1)(parseInt(digi)).toString('hex'))
   var bIpv4 = Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff].concat(ipHex))
   var bPort = utils.writeUIntBE(2)(port)
 
-  var chunks = [service, bIpv4, bPort]
-  chunks.forEach((buf) => {
-    addr = utils.suffixBy(buf)(addr)
-  })
+  var addr = utils.bufferConcat([service, bIpv4, bPort])
 
   return addr
 }
@@ -28,8 +23,6 @@ var address = (ip, port) => {
  * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#version
  */
 var version = (opts) => {
-  var ver = Buffer.alloc(0)
-
   var protocol = utils.writeUIntLE(4)(opts.protocol)
   var service = Buffer.from([0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
   var timestamp = utils.writeUIntLE(8)(opts.timestamp ? opts.timestamp : new Date().getTime())
@@ -40,66 +33,110 @@ var version = (opts) => {
   var subver = Buffer.from('/Satoshi:0.7.2/')
   var blockHeight = utils.writeUIntLE(4)(opts.blockHeight)
 
-  var chunks = [protocol, service, timestamp, addrYou, addrMe, nodeId, subverLength, subver, blockHeight]
-  chunks.forEach((buf) => {
-    ver = utils.suffixBy(buf)(ver)
-  })
+  var payload = utils.bufferConcat([protocol, service, timestamp, addrYou, addrMe, nodeId, subverLength, subver, blockHeight])
+  var header = addMessageHeader(opts.network, 'version', payload)
+  var message = utils.prefixBy(header)(payload)
 
-  var header = addMessageHeader(opts.network, 'version', ver)
-  ver = utils.prefixBy(header)(ver)
-
-  return ver
+  return message
 }
 
 /* Verack
  * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#verack
  */
 var verack = (opts) => {
-  var verack = Buffer.alloc(0)
-  return addMessageHeader(opts.network, 'verack', verack)
+  var payload = Buffer.alloc(0)
+  return addMessageHeader(opts.network, 'verack', payload)
 }
 
 /* Get Headers
  * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#getheaders
  */
 var getHeaders = (opts) => {
-  var msg = Buffer.alloc(0)
-
   var protocol = utils.writeUIntLE(4)(opts.protocol)
   var hashCount = utils.writeUIntLE(1)(1)
   var highestBlock = Blockchain.getHighestBlock()
   var highestBlockHash = Buffer.from(utils.reverseHex(highestBlock.hash), 'hex')
   var hashStop = Buffer.alloc(32)
 
-  var chunks = [protocol, hashCount, highestBlockHash, hashStop]
-  chunks.forEach((buf) => {
-    msg = utils.suffixBy(buf)(msg)
-  })
+  var payload = utils.bufferConcat([protocol, hashCount, highestBlockHash, hashStop])
+  var header = addMessageHeader(opts.network, 'getheaders', payload)
+  var message = utils.prefixBy(header)(payload)
 
-  var header = addMessageHeader(opts.network, 'getheaders', msg)
-  msg = utils.prefixBy(header)(msg)
+  return message
+}
 
-  return msg
+/* Inventory
+ * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#Inventory_Vectors
+ */
+var inventory = (hash) => {
+  var type = utils.writeUIntLE(4)(2)
+  var bHash = Buffer.from(utils.reverseHex(hash), 'hex')
+
+  var inv = utils.bufferConcat([type, bHash])
+
+  return inv
 }
 
 /* Get Data
  * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#getdata
  */
 var getData = (opts) => {
+  var invs = opts.hashes.map(inventory)
 
+  var payload = utils.bufferConcat([utils.writeVarInt(opts.hashes.length)].concat(invs))
+  var header = addMessageHeader(opts.network, 'getdata', payload)
+  var message = utils.prefixBy(header)(payload)
+
+  return message
+}
+
+/* Block Headers Data
+ * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#Block_Headers
+ */
+var blockHeader = (hex) => {
+  var header = Buffer.from(hex, 'hex')
+  return {
+    version: utils.slice(0, 4)(header),
+    prev_block: utils.slice(4, 36)(header),
+    merkle_root: utils.slice(36, 68)(header),
+    timestamp: utils.slice(68, 72)(header),
+    difficulty: utils.slice(72, 76)(header),
+    nonce: utils.slice(76, 80)(header),
+    txn_count: utils.slice(80, 81)(header)
+  }
 }
 
 /* Headers Data
  * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#headers
  */
-var headers = (headers) => {
+var headers = (raw) => {
+  var isStartedFD = bufferStartsWith(Buffer.from([0xFD]))
+  var isStartedFE = bufferStartsWith(Buffer.from([0xFE]))
+  var isStartedFF = bufferStartsWith(Buffer.from([0xFF]))
 
+  var bInt
+  var bHeaders
+  if (isStartedFF(raw)) {
+    bInt = raw.slice(0, 8)
+    bHeaders = raw.slice(8)
+  } else if(isStartedFE(raw)) {
+    bInt = raw.slice(0, 4)
+    bHeaders = raw.slice(4)
+  } else if(isStartedFD(raw)) {
+    bInt = raw.slice(0, 2)
+    bHeaders = raw.slice(2)
+  } else {
+    bInt = raw.slice(0, 1)
+    bHeaders = raw.slice(1)
+  }
+  var count = utils.readVarInt(bInt)
+  var headers = bHeaders.toString('hex').match(/.{81}/g).map(blockHeader)
 }
 
 /* Block Data
  * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#block
  */
-var block = (block) => {
+var block = (raw) => {
 
 }
 
@@ -107,17 +144,12 @@ var block = (block) => {
  * Doc => https://en.bitcoin.it/wiki/Protocol_documentation#Message_structure
  */
 var addMessageHeader = (network, command, payload) => {
-  var header = Buffer.alloc(0)
-
   var magic = Buffer.from(Magic[network.toUpperCase()])
   var cmd = Buffer.from(Command[command.toUpperCase()])
   var payloadLength = utils.writeUIntLE(4)(payload.length)
   var PayloadChecksum = utils.slice(0, 4)(utils.dsha256(payload))
 
-  var chunks = [magic, cmd, payloadLength, PayloadChecksum]
-  chunks.forEach((buf) => {
-    header = utils.suffixBy(buf)(header)
-  })
+  var header = utils.bufferConcat([magic, cmd, payloadLength, PayloadChecksum])
 
   return header
 }
@@ -128,7 +160,9 @@ module.exports = {
     verack: verack,
     address: address,
     getHeaders: getHeaders,
-    getData: getData
+    getData: getData,
+    inventory: inventory,
+    blockHeader: blockHeader
   },
   reader: {
     headers: headers,
