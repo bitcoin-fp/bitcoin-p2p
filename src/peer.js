4948a7ef1541e851582b2e0bbf132552eb56f06e
benzwjian <zwjian@gmail.com>
2017-07-11 09:43:05 +0800

    send message to peer

--- src/peer.js
+++ src/peer.js
@@ -1,6 +1,7 @@
 var net = require('net')
 var NETWORK = require('./const').NETWORK
 var PORT = require('./const').PORT
+var msgWriter = require('./message').writer
 
 var peerConnection = (network) => (ip) => {
   return new Promise((resolve, reject) => {
@@ -17,11 +18,24 @@ var peerConnection = (network) => (ip) => {
       console.log('peer ' + ip + ' connect fail')
       reject(err)
     })
+
+    socket.on('data', function (data) {
+      console.log('received data from ' + socket.remoteAddress)
+      console.log(data)
+    })
   })
+}
+
+var send = (socket) => {
+  console.log(socket.localAddress)
+  console.log(socket.remoteAddress)
+  var version = msgWriter.version({protocol: 60002, addrMe: socket.localAddress, addrYou: socket.remoteAddress, network: 'mainnet', blockHeight: 0})
+  socket.write(version)
 }
 
 var allPeersConnected = (peerSockets) => {
   console.log('All ' + peerSockets.length + ' peers connected!')
+  send(peerSockets[0])
 }
 
 var connect = (network) => (ips) => {
