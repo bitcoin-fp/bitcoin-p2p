3fffd45b38bec24fe25c324399aae40a41857f7d
benzwjian <zwjian@gmail.com>
2017-09-19 15:59:11 +0800

    start from dns resolving

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+var dns = require('./src/dns')
 var peer = require('./src/peer')
 var Blockchain = require('./src/blockchain')
 
 Blockchain.init('./db/headers').then(function (topHeight) {
-  // dns.resolve('mainnet')
   console.log('Bitcoin p2p network started. The local node has %d block(s).', topHeight)
-  peer.buildPool('mainnet')(['104.237.2.189'])
+  dns.resolve('mainnet')
+  // peer.buildPool('mainnet')(['104.237.2.189', '13.73.0.61']) // node for test
 })
